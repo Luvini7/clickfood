633c03fa82fd5dd6a24a83c41afdebaef95a93bf
Lucas Vinicius <lucasmartins2468@gmail.com>
2024-12-03 11:15:13 -0300

    Adiciona todos os arquivos novos dentro do repositório

---
+++ clickfood_project/clickfood/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ClickfoodConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'clickfood'
---
+++ clickfood_project/clickfood/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+def sobre(request):
+    html = '<html lang="pt-br"> <body> <h1> Discente: Lucas Vinicius David Martins </h1> <h2> Matrícula: 20221261020093 </h2> </body></html>'
+    return HttpResponse(html)
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # Autenticar o usuário
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            # Realizar login do usuário
+            login(request, user)
+            return redirect('home')  # Redireciona para a página inicial, altere conforme necessário
+        else:
+            messages.error(request, 'Credenciais inválidas. Tente novamente.')
+
+    return render(request, 'login.html')  # Retorna o formulário de login
